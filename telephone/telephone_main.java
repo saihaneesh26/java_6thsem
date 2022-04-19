02843ede0ee47913d57fdee7f84aeb4b4e132ac5
saihaneesh26 <56464193+saihaneesh26@users.noreply.github.com>
2022-04-19 20:05:06 +0530

    Create telephone_main.java

---
+++ telephone/telephone_main.java
@@ -0,0 +1,12 @@
+package telephone;
+import telephone.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		contactList cl = new contactList();
+		missedCallList ml = new missedCallList();
+		ml.viewLog();
+	}
+
+}
