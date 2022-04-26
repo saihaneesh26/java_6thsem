a8d78e45ae20cc42def1246946caa75c06a612f3
saihaneesh26 <56464193+saihaneesh26@users.noreply.github.com>
2022-04-26 10:13:21 +0530

    Create Customer.java

---
+++ swings_customer/Customer.java
@@ -0,0 +1,18 @@
+package swings_customer;
+
+public class Customer {
+	
+	String cid,username,pass;
+	int phone;
+	
+	public Customer(String username,String pass,String cid,int phone) {
+		this.cid = cid;
+		this.phone = phone;
+		this.pass = pass;
+		this.username = username;
+	}
+	
+	public String toString() {
+		return "Username :"+username+"CID: "+cid+" PHONE: "+phone;
+	}
+}
