2568f643867283c6da05100b13b8c663a9b982ff
saihaneesh26 <56464193+saihaneesh26@users.noreply.github.com>
2022-04-26 09:10:52 +0530

    Update ContactList.java

--- telephone/ContactList.java
+++ telephone/ContactList.java
@@ -1,30 +1,18 @@
-package telephone;
+package Phone;
 import java.util.*;
-import telephone.contacts;
 
-public class contactList {
-
-	Scanner in = new Scanner(System.in);
-	static ArrayList<telephone.contacts> c = new ArrayList<telephone.contacts>();
-	public contactList(){
-		while(true) {
-			System.out.println("Enter 1 to enter else 0");
-			if(in.nextLine() == "1") {
-				System.out.println("Enter name ");
-				String name = in.nextLine();
-				System.out.println("Enter phno ");
-				long phno = in.nextLong();
-				in.nextLine();
-				telephone.contacts cs = new telephone.contacts(name, phno);
-				c.add(cs);
-				
-				
-			}else {
-				break;
-			}
-		}
-	}
-	public static ArrayList<contacts> getclist(){
-		return c;
+public class ContactList {
+	Scanner s = new Scanner(System.in);
+	static ArrayList<Contacts> clist = new ArrayList<Contacts>();
+	public ContactList() {
+		clist.add(new Contacts(1,"a"));
+		clist.add(new Contacts(2,"b"));
+		clist.add(new Contacts(3,"c"));
+		clist.add(new Contacts(4,"d"));
+		clist.add(new Contacts(5,"e"));
+		clist.add(new Contacts(6,"f"));
+	}	
+	public static ArrayList<Contacts> getclist(){
+		return clist;
 	}
 }
